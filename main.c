75179ee5dff6cbe725d0a5ec3fabb4d5ce568135
ginsabu <155948685+ginsabu@users.noreply.github.com>
2025-11-09 22:57:35 +0900

    pr5

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-#include <string.h>
 
-void main(void) {
-    char str[30] = "happy C programming";
+int main(void) {
+    char word1[50];
+    char word2[50];
+    char word3[50];
+    FILE *fp;
 
-    printf("The length of \"%s\" is : %i\n", str, strlen(str));
+    printf("input a word: ");
+    scanf("%s", word1);
+
+    printf("input a word: ");
+    scanf("%s", word2);
+
+    printf("input a word: ");
+    scanf("%s", word3);
+
+    fp = fopen("sample.txt", "w");
+    if (fp == NULL) {
+        printf("File open error!\n");
+        return 1;
+    }
+
+    fprintf(fp, "%s\n", word1);
+    fprintf(fp, "%s\n", word2);
+    fprintf(fp, "%s\n", word3);
+
+    fclose(fp);
+
+    return 0;
 }
